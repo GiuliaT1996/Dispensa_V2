f5cdcfa929a82f76ed33aebb4e7137c4e3a29b8d
Giulia.Tincano <giulia.tincano@wolterskluwer.com>
2023-10-07 19:20:16 +0200

    added optional shopping list

--- app/src/main/java/com/angiuprojects/dispensav2/activities/implementation/ShoppingListActivity.kt
+++ app/src/main/java/com/angiuprojects/dispensav2/activities/implementation/ShoppingListActivity.kt
@@ -1,6 +1,9 @@
 package com.angiuprojects.dispensav2.activities.implementation
 
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
+import androidx.recyclerview.widget.RecyclerView
 import com.angiuprojects.dispensav2.R
 import com.angiuprojects.dispensav2.activities.BaseActivity
 import com.angiuprojects.dispensav2.adapters.ShoppingListUnitRecyclerAdapter
@@ -20,14 +23,31 @@ class ShoppingListActivity : BaseActivity<ActivityShoppingListBinding>(ActivityS
             isSearchPresent = false
         )
 
-        Utils.singleton.setRecyclerAdapter(findViewById(R.id.shopping_list),
+        binding.expandMandatory.setOnClickListener { expand(binding.expandMandatory, binding.shoppingListMandatory) }
+        binding.expandOptional.setOnClickListener { expand(binding.expandOptional, binding.shoppingListOptional) }
+
+        Utils.singleton.setRecyclerAdapter(findViewById(R.id.shopping_list_mandatory),
             this,
-            ShoppingListUnitRecyclerAdapter(filterShoppingList()))
+            ShoppingListUnitRecyclerAdapter(filterShoppingList(isOptional = false)))
+
+        Utils.singleton.setRecyclerAdapter(findViewById(R.id.shopping_list_optional),
+            this,
+            ShoppingListUnitRecyclerAdapter(filterShoppingList(isOptional = true)))
     }
 
-    private fun filterShoppingList() : MutableList<StorageItem> {
-        //TODO add optional (trigger = -1)
+    private fun filterShoppingList(isOptional: Boolean) : MutableList<StorageItem> {
         return Constants.itemMapFilteredByProfile
-            .filter { it.value.quantity <= it.value.trigger }.values.toMutableList()
+            .filter { (isOptional && it.value.quantity == 0 && it.value.trigger < 0)
+                    || (!isOptional && it.value.quantity <= it.value.trigger) }.values.toMutableList()
+    }
+
+    private fun expand(expandButton: ImageButton, recyclerView: RecyclerView) {
+        if(recyclerView.visibility == View.VISIBLE) {
+            expandButton.rotation = -90F
+            recyclerView.visibility = View.GONE
+        } else {
+            expandButton.rotation = 0F
+            recyclerView.visibility = View.VISIBLE
+        }
     }
 }
